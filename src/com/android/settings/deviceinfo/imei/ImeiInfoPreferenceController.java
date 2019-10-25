03e291f4240934f2a30713da788ee4aefc0c0b72
El Dainosor <eldainosor@gmail.com>
2019-10-25 07:16:45 +0000

    settings: Protect sensitive info on About device
    Tired of seeing people/see yourself editing every screenshot to hide some personal info like their phone numbers or their IMEI? Well, this commit will help by adding a custom string to inform the user that, to get that info, they should tap that preference. If you want to show them, you can make an overlay and set configShowDeviceSensitiveInfo as true.
    
    Change-Id: I45b7898da440c36045aec64a0a86fb804116cd9d

--- src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -150,8 +150,11 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
                 simSlot = 0;
             }
         }
-        return phoneType == PHONE_TYPE_CDMA ? mTelephonyManager.getMeid(simSlot)
-                : mTelephonyManager.getImei(simSlot);
+        if (mContext.getResources().getBoolean(R.bool.configShowDeviceSensitiveInfo)) {
+            return phoneType == PHONE_TYPE_CDMA ? mTelephonyManager.getMeid(simSlot)
+                    : mTelephonyManager.getImei(simSlot);
+        }
+        return mContext.getString(R.string.device_info_protected_single_press);
     }
 
     @Override

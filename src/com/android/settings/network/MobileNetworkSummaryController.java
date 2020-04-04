a817c94f0ac84711042c8c13b0f08d3097431624
rituj <ritujbeniwal@gmail.com>
2020-04-04 09:29:40 +0530

    Settings: Remove mobile network preference if V2 is enabled
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: I8bf7f2f792ca2cec5582bdc1d3f2c06bd15ba103

--- src/com/android/settings/network/MobileNetworkSummaryController.java
+++ src/com/android/settings/network/MobileNetworkSummaryController.java
@@ -28,7 +28,9 @@ import android.telephony.SubscriptionManager;
 import android.telephony.euicc.EuiccManager;
 
 import com.android.settings.R;
+import com.android.settings.core.FeatureFlags;
 import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.development.featureflags.FeatureFlagPersistent;
 import com.android.settings.network.telephony.MobileNetworkActivity;
 import com.android.settings.network.telephony.MobileNetworkUtils;
 import com.android.settings.widget.AddPreference;
@@ -180,7 +182,8 @@ public class MobileNetworkSummaryController extends AbstractPreferenceController
 
     @Override
     public boolean isAvailable() {
-        return !Utils.isWifiOnly(mContext) && mUserManager.isAdminUser();
+        return !Utils.isWifiOnly(mContext) && mUserManager.isAdminUser()
+            && !FeatureFlagPersistent.isEnabled(mContext, FeatureFlags.NETWORK_INTERNET_V2);
     }
 
     @Override

88e5c5b896215abeb2733c4a9fe944482766218f
ezio84 <brabus84@gmail.com>
2020-04-25 11:18:47 +0200

    Remove aosp battery percentage option
    Change-Id: Ie36b594cbcde7777470cd698ab40cd2aaf3f004c

--- src/com/android/settings/display/BatteryPercentagePreferenceController.java
+++ src/com/android/settings/display/BatteryPercentagePreferenceController.java
@@ -54,12 +54,10 @@ public class BatteryPercentagePreferenceController extends BasePreferenceControl
 
     @Override
     public int getAvailabilityStatus() {
-        if (!Utils.isBatteryPresent(mContext)) {
-            return CONDITIONALLY_UNAVAILABLE;
-        }
-        return mContext.getResources().getBoolean(
+
+        return /*mContext.getResources().getBoolean(
                 R.bool.config_battery_percentage_setting_available) ? AVAILABLE
-                : UNSUPPORTED_ON_DEVICE;
+                : */UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
@@ -74,7 +72,7 @@ public class BatteryPercentagePreferenceController extends BasePreferenceControl
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         boolean showPercentage = (Boolean) newValue;
         Settings.System.putInt(mContext.getContentResolver(), SHOW_BATTERY_PERCENT,
-                showPercentage ? 1 : 0);
+                showPercentage ? 0 : 0);
         return true;
     }
 }

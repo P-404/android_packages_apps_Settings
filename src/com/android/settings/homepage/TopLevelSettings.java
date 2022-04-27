674d4c5b83ca30746c632731f96315781a610d11
hpnightowl <iamhp2k@gmail.com>
2022-04-27 10:02:47 +0000

    Settings: Redoing dashboard icons 404 Way!
    Change-Id: Ia75e9ad3cc078cea76ba7bb81656ca3ba2d3315b

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -172,22 +172,6 @@ public class TopLevelSettings extends DashboardFragment implements
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
         final PreferenceScreen screen = getPreferenceScreen();
-        if (screen == null) {
-            return;
-        }
-        // Tint the homepage icons
-        final int tintColor = Utils.getHomepageIconColor(getContext());
-        final int count = screen.getPreferenceCount();
-        for (int i = 0; i < count; i++) {
-            final Preference preference = screen.getPreference(i);
-            if (preference == null) {
-                break;
-            }
-            final Drawable icon = preference.getIcon();
-            if (icon != null) {
-                icon.setTint(tintColor);
-            }
-        }
     }
 
     @Override
